28efacdc49430a0c72429b087cb573fa4fb7d102
Piotr Młudzik <piotr.mludzik@gmail.com>
2021-01-16 00:29:08 +0100

    ant soldier can move

--- src/main/java/com/codecool/ants/ants/Soldier.java
+++ src/main/java/com/codecool/ants/ants/Soldier.java
@@ -1,8 +1,13 @@
 package com.codecool.ants.ants;
 
+import com.codecool.ants.geometry.Direction;
+import com.codecool.ants.geometry.Position;
 import com.codecool.ants.logic.Field;
 
 public class Soldier extends Ant {
+
+    private final Direction[ ] directions = new Direction[ ]{Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST};
+    private int directionIndex = 0;
 
     public Soldier(Field field) {
         super(field, 'S');
@@ -10,7 +15,22 @@ public class Soldier extends Ant {
 
     @Override
     public void move() {
-        // TODO: implement the ant soldier move.
-        super.move();
+        Position actualPosition = new Position(super.getPosition().x, super.getPosition().y);
+
+        super.move(directions[directionIndex]);
+        if (didNotMove(actualPosition))
+            return;
+
+        directionIndex++;
+        if (MovedAround())
+            directionIndex = 0;
+    }
+
+    private boolean didNotMove(Position lastPosition) {
+        return lastPosition.x == super.getPosition().x && lastPosition.y == super.getPosition().y;
+    }
+
+    private boolean MovedAround() {
+        return directionIndex == directions.length;
     }
 }
