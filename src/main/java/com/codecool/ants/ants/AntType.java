6143f270c0c251bd57d54c5b746dc74d8f4b4691
Piotr Młudzik <piotr.mludzik@gmail.com>
2021-01-19 15:42:24 +0100

    code simplification

--- src/main/java/com/codecool/ants/ants/AntType.java
+++ src/main/java/com/codecool/ants/ants/AntType.java
@@ -1,14 +1,8 @@
 package com.codecool.ants.ants;
 
 public enum AntType {
-    QUEEN("Queen"),
-    WORKER("Worker"),
-    SOLDIER("Soldier"),
-    DRONE("Drone");
-
-    public final String simpleName;
-
-    AntType(String simpleName) {
-        this.simpleName = simpleName;
-    }
+    QUEEN,
+    WORKER,
+    SOLDIER,
+    DRONE;
 }
