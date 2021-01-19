9883bf1893c87395b4742ebb29be6e3b60ab8551
Piotr Młudzik <piotr.mludzik@gmail.com>
2021-01-19 15:13:46 +0100

    style the code

--- src/main/java/com/codecool/ants/geometry/Direction.java
+++ src/main/java/com/codecool/ants/geometry/Direction.java
@@ -1,16 +1,16 @@
 package com.codecool.ants.geometry;
 
-    public enum Direction {
-        NORTH( 0,+1),
-        EAST (+1, 0),
-        SOUTH( 0,-1),
-        WEST (-1, 0);
+public enum Direction {
+    NORTH( 0,+1),
+    EAST (+1, 0),
+    SOUTH( 0,-1),
+    WEST (-1, 0);
 
-        public final int vectorX;
-        public final int vectorY;
+    public final int vectorX;
+    public final int vectorY;
 
-        Direction(int vectorX, int vectorY) {
-            this.vectorX = vectorX;
-            this.vectorY = vectorY;
-        }
+    Direction(int vectorX, int vectorY) {
+        this.vectorX = vectorX;
+        this.vectorY = vectorY;
     }
+}
